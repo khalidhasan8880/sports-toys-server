5ca2b8b955a6ce9db8ac9f46237274425b4002b0
khalidhasan8880 <khalidhasan8880@gmail.com>
2023-05-18 17:55:10 +0600

    added mongodb database user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 const cors = require('cors')
 require('dotenv').config()
@@ -12,12 +13,38 @@ app.use(cors())
 
 
 
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.sgqndpo.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const toysCollection = client.db("sportsToysDB").collection("toys");
+    
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
 
 
 
 
 
-app.get('', (req, res) => {
+
+app.get('/', (req, res) => {
   res.send('ok ok')
 })
 
